11037f4c47e0d7fbbba332c0e712b222315804be
Andrew Druchylo <androbene@ua.fm>
2021-11-08 09:31:57 +0200

    Initial commit

---
+++ app/src/main/java/com/androbene/a888_git_branches_study/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androbene.a888_git_branches_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/androbene/a888_git_branches_study/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.androbene.a888_git_branches_study
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
