acbc9969081d1d058e1570b17ff0b2637ab61d1e
Andrew Druchylo <androbene@ua.fm>
2022-01-23 22:12:01 +0200

    Commit-3

--- app/src/main/java/com/androbene/a888_git_branches_study/MainActivity.kt
+++ app/src/main/java/com/androbene/a888_git_branches_study/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.androbene.a888_git_branches_study
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val w = 0
     }
 }
